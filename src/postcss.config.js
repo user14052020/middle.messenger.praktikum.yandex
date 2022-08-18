06fff6bb18b5f4cac5800ae1a017dcf18b76df04
Vadim Maharram <asktask@icloud.com>
2022-08-18 17:21:47 +0500

    Настройка Netlify

--- src/postcss.config.js
+++ src/postcss.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     plugins: [
         require('precss')({/* ...options */}),
-        require('autoprefixer')({/* ...options */}),
+        require('autoprefixer')({ browsers: ['last 2 versions', 'ie 6-8', 'Firefox > 20']  }),
 
     ]
 }
