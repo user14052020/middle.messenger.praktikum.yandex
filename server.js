70a250bfe861291d337da36f48b88668b2bbc83f
Vadim Maharram <asktask@icloud.com>
2022-08-19 11:50:34 +0500

    bug fix

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-process.env.PORT || PORT;
-
 app.use(express.static(__dirname + '/dist'));
 
 app.get('*', (req, res) => {
