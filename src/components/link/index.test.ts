1a78ea25a37bf5d326af4a3fbe6c69754e841dde
Vadim Maharram <asktask@icloud.com>
2022-10-07 18:21:29 +0500

    bug fix

--- src/components/link/index.test.ts
+++ src/components/link/index.test.ts
@@ -2,6 +2,7 @@ import { Link,LinkProps } from './index';
 import { expect } from 'chai';
 import Router from '../../utils/Router';
 import sinon from 'sinon';
+import {AuthorizationRegistrationPage} from "../../pages/authorization_registration";
 
 
 
@@ -18,10 +19,12 @@ describe.only('Link', () => {
     });
 
     it('should go to passed route on click', () => {
+        Router.use('/',AuthorizationRegistrationPage);
+        Router.start();
         const link = new Link({label:"Выход", events: {click: () => {Router.go('/')}}} as LinkProps);
         const spy = sinon.spy(Router, 'go');
         const element = link.element as HTMLDivElement;
-
+        console.log(element)
         element.click();
 
         expect(spy.calledOnce).to.eq(true);
